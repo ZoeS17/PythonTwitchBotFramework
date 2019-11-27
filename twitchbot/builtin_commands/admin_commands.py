07a664768d40d165e41983d32302a3a305282a3b
sharkbound <laptopblaster@gmail.com>
2019-11-27 10:30:36 -0600

    added SHUTDOWN_TIME to admincommands for use ?s ?stop and ?shutdown

--- twitchbot/builtin_commands/admin_commands.py
+++ twitchbot/builtin_commands/admin_commands.py
@@ -3,6 +3,7 @@ import asyncio
 from twitchbot import Command, CommandContext, Message, get_bot, stop_all_tasks
 
 ADMIN_COMMAND_PERMISSION = 'admin'
+SHUTDOWN_TIME = 3
 
 
 @Command('shutdown', aliases=['stop', 's'], context=CommandContext.BOTH, permission=ADMIN_COMMAND_PERMISSION,
@@ -11,7 +12,7 @@ async def cmd_shutdown(msg: Message, *args):
     await msg.reply('bot shutting down...')
     stop_all_tasks()
 
-    for seconds_left in range(10, 0, -1):
+    for seconds_left in range(SHUTDOWN_TIME, 0, -1):
         print(f'giving running tasks time to stop, closing in {seconds_left} seconds...')
         await asyncio.sleep(1)
 
